bfbb32808bdd70a54bc3e59944f068a98aea2263
Vanessa Rice <136823162+infuriated-mink@users.noreply.github.com>
2023-08-12 19:11:55 -0230

    input custom report

--- main.py
+++ main.py
@@ -40,13 +40,20 @@ def newEmployee():
                 print("Enter a valid name please.")
             else:
                 break
-
         while True:
             empLastName = input("Enter employee last name: ").title()
             if not set(empLastName).issubset(empNameAllowed):
                 print("Enter a valid name please.")
             elif empLastName == "":
                 print("Enter a valid name please.")
+            else:
+                break
+
+
+        while True:
+            empAddress = input("Enter employee address: ")
+            if empAddress == "":
+                print("Enter a valid address.")
             else:
                 break
 
@@ -61,16 +68,9 @@ def newEmployee():
                 break
 
         while True:
-            empAddress = input("Enter employee address: ")
-            if empAddress == "":
-                print("Enter a valid address.")
-            else:
-                break
-
-        while True:
             empPostalCharAllowed = set("ASDFGHJKLQWERTYUIOPZXCVBNM")
             empPostalNumAllowed = set("1234567890")
-            empPostal = input("Enter employee postal code: ").upper()
+            empPostal = input("Enter employee postal code(X1X1X1): ").upper()
             if empPostal == "":
                 print("Enter a valid Postal Code.")
             elif len(empPostal) != 6:
@@ -84,7 +84,7 @@ def newEmployee():
 
         while True:
             provAllowed = ["NL", "ON", "ONT", "QC", "SK", "MB", "NS", "NB", "PEI", "AB", "BC", "NWT", "NT", "YK"]
-            empProv = input("Enter employee province: ").upper()
+            empProv = input("Enter employee province(XX): ").upper()
             if empProv not in provAllowed:
                 print("Enter a valid province please.")
             elif empProv == "":
@@ -94,7 +94,7 @@ def newEmployee():
 
         while True:
             phoneNumAllowed = set("1234567890-")
-            empPhone = input("Enter employee phone number: ")
+            empPhone = input("Enter employee phone number(999-999-9999): ")
             if not set(empPhone).issubset(phoneNumAllowed):
                 print("Enter a valid number please. ")
             elif len(empPhone) != 12:
@@ -162,7 +162,7 @@ def newEmployee():
 
         balDueTotal = balDueSubtotal * (1 + HST_RATE)
 
-        f = open("Drivers.dat", "a")
+        f = open("drivers.dat", "a")
 
         driverFirstName = f.write(f"{empFirstName}, ")
         driverLastName = f.write(f"{empLastName}, ")
@@ -193,8 +193,101 @@ def newEmployee():
 # def companyProfitListing():
 
 
-# def customReport():
+def customReport():
+    """
+    Generates a custom financial report for a specific driver within a given date range.
 
+    Parameters:
+    - driver_number_input (str): The driver number to generate the report for. Enter '0' to return to the main menu.
+    - target_start_date (str): The start date of the date range (YYYY-MM-DD).
+    - target_end_date (str): The end date of the date range (YYYY-MM-DD).
+
+    Outputs:
+    - Displays a report showing transaction details, payments, and remaining balance for the selected driver
+      within the specified date range. If the driver number is '0', the function returns to the main menu.
+    """
+    driver_data = {}
+    with open('drivers.dat', 'r') as f:
+        next(f)  # Skip the header line
+        for line in f:
+            data = line.strip().split(',')
+            driver_id = int(data[0])
+            driver_info = {
+                'LicenseNum': data[1],
+                'CarID': data[2],
+                'EmpName': data[3],
+                'EmpStreetAdd': data[4],
+                'EmpCity': data[5],
+                'EmpProv': data[6],
+                'EmpPhone': data[7],
+                'EmpEmail': data[8],
+                'BalDue': float(data[9]) if data[9] != 'Yes' else 0.0,
+            }
+            driver_data[driver_id] = driver_info
+
+    revenue_data = []
+    with open('revenue.dat', 'r') as f:
+        next(f)  # Skip the header line
+        for line in f:
+            data = line.strip().split(',')
+            transaction_id = int(data[0])
+            date = data[1]
+            payment_description = data[2]
+            driver_id = int(data[3])
+            subtotal = float(data[4])
+            hst = float(data[5])
+            total = float(data[6])
+            revenue_info = {
+                'payment_description': payment_description,
+                'subtotal': subtotal,
+                'hst': hst,
+                'total': total
+            }
+            revenue_data.append({
+                'transaction_id': transaction_id,
+                'date': date,
+                'driver_id': driver_id,
+                'revenue_info': revenue_info
+            })
+
+    while True:
+        driver_number_input = input("Enter driver number (or 0 to return to the main menu): ")
+        if driver_number_input == '0':
+            break
+
+        try:
+            driver_number = int(driver_number_input)
+        except ValueError:
+            print("Invalid input. Please enter a valid driver number or 0 to return to the main menu.")
+            continue
+        
+        target_start_date = input("Enter the start date of the range (YYYY-MM-DD): ")
+        target_end_date = input("Enter the end date of the range (YYYY-MM-DD): ")
+
+        if driver_number in driver_data:
+            driver_info = driver_data[driver_number]
+            print(f"Report for Driver {driver_number}: {driver_info['EmpName']}")
+            print("Date\t\tTransaction ID\tPayment Description\tSubtotal\tHST\tTotal")
+
+            total_payments = 0
+
+            for entry in revenue_data:
+                if entry['driver_id'] == driver_number and target_start_date <= entry['date'] <= target_end_date:
+                    revenue_info = entry['revenue_info']
+                    print(f"{entry['date']}\t{entry['transaction_id']}\t\t{revenue_info['payment_description']}\t\t{revenue_info['subtotal']:.2f}\t\t{revenue_info['hst']:.2f}\t{revenue_info['total']:.2f}")
+                    total_payments += revenue_info['total']
+
+            print("\nSummary:")
+            remaining_balance = driver_info['BalDue'] - total_payments
+            if remaining_balance < 0:
+                remaining_balance = 0
+            print(f"Total payments within the date range: ${total_payments:.2f}")
+            print(f"Remaining balance owing: ${remaining_balance:.2f}")
+        else:
+            print(f"Driver {driver_number} not found.")
+
+    if __name__ == "__main__":
+        customReport()
 
 # main program
 

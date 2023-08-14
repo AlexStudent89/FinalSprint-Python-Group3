b72316948640940ca27fb6ec91bc9e767a807516
Vanessa Rice <vanessa.rice@keyin.com>
2023-08-14 20:39:24 -0230

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 import csv
 import datetime
 
+
+
 # Constants
 f = open('defaults.dat', 'r')
 
@@ -19,8 +21,46 @@ HST_RATE = float(f.readline())
 
 f.close()
 
+# Functions
+def read_expenses_data(filename):
+    expenses = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            data = line.strip().split('\t') 
+            expenses.append({
+                "InvNum": int(data[0]),
+                "DriverID": int(data[1]),
+                "CarID": int(data[2]),
+                "InvDate": data[3],
+                "ItemName": data[4],
+                "ItemNum": int(data[5]),
+                "Description": data[6],
+                "Quantity": int(data[7]),
+                "UnitCost": float(data[8]),
+                "Subtotal": float(data[9]),
+                "HST": float(data[10]),
+                "Total": float(data[11])
+            })
+    return expenses
 
-# functions
+def read_revenue_data(filename):
+    revenue = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            data = line.strip().split(',')
+            revenue.append({
+                "TransactionID": int(data[0]),
+                "Date": data[1],
+                "PaymentDescription": data[2],
+                "DriverID": int(data[3]),
+                "Subtotal": float(data[4]),
+                "HSTamt": float(data[5]),
+                "Total": float(data[6])
+            })
+    return revenue
+
 def newEmployee():
     while True:
 
@@ -190,153 +230,65 @@ def newEmployee():
 # def carRentals():
 
 
-# def companyProfitListing():
+def companyProfitListing(start_date, end_date):
+    expenses = read_expenses_data("expenses.dat")
+    revenue = read_revenue_data("revenue.dat")
 
+    total_revenue = sum(rev["HSTamt"] for rev in revenue)
+    revenue_breakdown = {f"Revenue {i+1}": rev["HSTamt"] for i, rev in enumerate(revenue)}
 
-def customReport(): 
-    '''
-    This report takes payments made from revenue.dat file and driver information from the drivers.dat file
-    within specified dates and then creates a report to display payments made by drivers within the specified dates.
-    '''
-    driver_data = {}
-    print()
-    with open('drivers.dat', 'r') as f:
-        next(f) 
-        for line in f:
-            data = line.strip().split(',')
-            driver_id = int(data[0])
-            driver_info = {
-                'LicenseNum': data[1],
-                'CarID': data[2],
-                'EmpName': data[3],
-                'EmpStreetAdd': data[4],
-                'EmpCity': data[5],
-                'EmpProv': data[6],
-                'EmpPhone': data[7],
-                'EmpEmail': data[8],
-                'BalDue': float(data[9]) if data[9] != 'Yes' else 0.0,
-            }
-            driver_data[driver_id] = driver_info
+    total_expenses = sum(expense["Quantity"] * expense["UnitCost"] + expense["HST"] for expense in expenses)
+    expenses_breakdown = {f"Expenses {i+1}": expense["Quantity"] * expense["UnitCost"] + expense["HST"] for i, expense in enumerate(expenses)}
 
-    revenue_data = []
-    with open('revenue.dat', 'r') as f:
-        next(f)  
-        for line in f:
-            data = line.strip().split(',')
-            transaction_id = int(data[0])
-            date = data[1]
-            payment_description = data[2]
-            driver_id = int(data[3])
-            subtotal = float(data[4])
-            hst = float(data[5])
-            total = float(data[6])
-            revenue_info = {
-                'payment_description': payment_description,
-                'subtotal': subtotal,
-                'hst': hst,
-                'total': total
-            }
-            revenue_data.append({
-                'transaction_id': transaction_id,
-                'date': date,
-                'driver_id': driver_id,
-                'revenue_info': revenue_info
-            })
+    total_profit_loss = total_revenue - total_expenses
 
+    print("HAB Taxi Services - Profit Listing Report")
+    print(f"Report Period: {start_date} to {end_date}")
+    print("\nRevenues:")
+    print(f"     Total Revenue: ${total_revenue:.2f}")
+    print("     Revenue Breakdown:")
+    for category, amount in revenue_breakdown.items():
+        print(f"          {category}: ${amount:.2f}")
+
+    print("\n\nDescription:\n\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\n")
+    print("Expenses:")
+    print(f"    Total Expenses: ${total_expenses:.2f}")
+    print("    Expenses Breakdown:")
+    for category, amount in expenses_breakdown.items():
+        print(f"        {category}: ${amount:.2f}")
+
+    print("\n\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\n")
+    print(f"Profit Loss: ${total_profit_loss:.2f}\n")
+
+# Main program
+if __name__ == "__main__":
     while True:
-        driver_number_input = input("Enter driver number (or 0 to return to the main menu): ")
-        if driver_number_input == '0':
-            break
+        print()
+        print("           HAB Taxi Services")
+        print("        Company Services System")
+        print()
+        print("1. Enter a New Employee (driver)")
+        print("2. Enter Company Revenues")
+        print("3. Enter Company Expenses")
+        print("4. Track Car Rentals")
+        print("5. Record Employee Payment")
+        print("6. Print Company Profit Listing")
+        print("7. Print Driver Financial Listing")
+        print("8. Custom report")
+        print("9. Quit Program")
+        print()
 
         try:
-            driver_number = int(driver_number_input)
-        except ValueError:
-            print("Invalid input. Please enter a valid driver number or 0 to return to the main menu.")
-            continue
-        
-        target_start_date = input("Enter the start date of the range (YYYY-MM-DD): ")
-        target_end_date = input("Enter the end date of the range (YYYY-MM-DD): ")
-        print()
-        if driver_number in driver_data:
-            driver_info = driver_data[driver_number]
-            company_name = "HAB Taxi Service"
-            print("{:^103s}".format(company_name))
-            print("-" * 103)
-            print(f"Report for Driver: {driver_number} ")
-            print(f"Employee Name: {driver_info['EmpName']}")
-            print(f"Address: {driver_info['EmpStreetAdd']}, {driver_info['EmpCity']}, {driver_info['EmpProv']}")
-            print()
-            print("Date          Transaction ID          Payment Description          Subtotal          HST          Total")
-            print("-" * 103)
-
-            total_payments = 0
-
-            for entry in revenue_data:
-                if entry['driver_id'] == driver_number and target_start_date <= entry['date'] <= target_end_date:
-                    revenue_info = entry['revenue_info']
-                    print("{:<13s}      {:<18d} {:<25s}     {:<15.2f} {:<11.2f}  {:<11.2f}".format(
-                        entry['date'], entry['transaction_id'], revenue_info['payment_description'],
-                        revenue_info['subtotal'], revenue_info['hst'], revenue_info['total']))
-                    total_payments += revenue_info['total']
-
-            print("-" * 103)
-            remaining_balance = driver_info['BalDue'] - total_payments
-            if remaining_balance < 0:
-                remaining_balance = 0
-            print(f"Total payments within the date range: ${total_payments:.2f}")
-            print(f"Remaining balance owing: ${remaining_balance:.2f}")
-            print()
-        else:
-            print(f"Driver {driver_number} not found.")
-
-    if __name__ == "__main__":
-        customReport()
-
-
-
-# main program
-
-while True:
-    print()
-    print("           HAB Taxi Services")
-    print("        Company Services System")
-    print()
-    print("1. Enter a New Employee (driver)")
-    print("2. Enter Company Revenues")
-    print("3. Enter Company Expenses")
-    print("4. Track Car Rentals")
-    print("5. Record Employee Payment")
-    print("6. Print Company Profit Listing")
-    print("7. Print Driver Financial Listing")
-    print("8. Custom report")
-    print("9. Quit Program")
-    print()
-
-    try:
-        userInput = int(input("Enter a number of 1 through 9: "))
-    except:
-        print("Please enter a valid number.")
-    else:
-        if 9 < userInput < 1:
+            userInput = int(input("Enter a number of 1 through 9: "))
+        except:
             print("Please enter a valid number.")
         else:
             if userInput == 1:
                 newEmployee()
-            elif userInput == 2:
-                print("Option not configured.")
-            elif userInput == 3:
-                print("Option not configured.")
-            elif userInput == 4:
-                carRentals()
-            elif userInput == 5:
-                employeePayment()
             elif userInput == 6:
-                companyProfitListing()
-            elif userInput == 7:
-                print("Option not configured.")
-            elif userInput == 8:
-                customReport()
-            else:
-                print()
-                print("System entering sleep mode. Thanks for using the company system.")
+                start_date = input("Enter the start date (YYYY-MM-DD): ")
+                end_date = input("Enter the end date (YYYY-MM-DD): ")
+                companyProfitListing(start_date, end_date)
+            elif userInput == 9:
+                print("\nSystem entering sleep mode. Thanks for using the company system.")
                 break
